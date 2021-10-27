0924af6b7666dde38d90dab2b161549678a6ee89
Lula <mlrosalesg@gmail.com>
2021-10-27 10:53:24 -0500

    Added favorite filtering logic

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,10 +5,15 @@ import '../widgets/product_item.dart';
 import '../providers/products.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavorites;
+
+  ProductsGrid(this.showFavorites);
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products =
+        showFavorites ? productsData.favoriteItems : productsData.items;
     return GridView.builder(
         padding: const EdgeInsets.all(10),
         itemCount: products.length,
@@ -20,11 +25,7 @@ class ProductsGrid extends StatelessWidget {
         ),
         itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
               value: products[i],
-              child: ProductItem(
-                  // products[i].id,
-                  // products[i].title,
-                  // products[i].imageUrl,
-                  ),
+              child: ProductItem(),
             ));
   }
 }
