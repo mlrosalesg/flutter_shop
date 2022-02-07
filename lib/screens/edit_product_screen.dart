dabf5e12abecf1c0158c42862607728f3a222c31
Lula <mlrosalesg@gmail.com>
2022-02-07 16:05:31 -0500

    Added image preview to edit product screen

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -8,7 +8,37 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  // Need to have one for each field
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  //String _imageUrl = '';
+  final _imageUrlFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+      print('lost focus');
+      print(_imageUrlController.text);
+    }
+  }
+
+  // Always dispose the focus nodes after using!!
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+    _imageUrlFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,11 +63,53 @@ class _EditProductScreenState extends State<EditProductScreen> {
               keyboardType: TextInputType.number,
               textInputAction: TextInputAction.next,
               focusNode: _priceFocusNode,
+              onFieldSubmitted: (_) {
+                FocusScope.of(context).requestFocus(_descriptionFocusNode);
+              },
             ),
             TextFormField(
               decoration: InputDecoration(label: Text('Description')),
               maxLines: 3,
               keyboardType: TextInputType.multiline,
+              focusNode: _descriptionFocusNode,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(top: 8, right: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey)),
+                  child: _imageUrlController.text.isEmpty
+                      //child: _imageUrl.isEmpty
+                      ? Text('Enter a URL')
+                      : Image.network(
+                          _imageUrlController.text,
+                          //_imageUrl,
+                          fit: BoxFit.cover,
+                          errorBuilder: (_, __, ___) => Align(
+                            child: Text('😢'),
+                            alignment: Alignment.center,
+                          ),
+                        ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(label: Text('Image URL')),
+                    keyboardType: TextInputType.url,
+                    textInputAction: TextInputAction.done,
+                    focusNode: _imageUrlFocusNode,
+                    controller: _imageUrlController,
+                    onFieldSubmitted: (String value) {
+                      setState(() {
+                        //_imageUrl = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
           ],
         )),
